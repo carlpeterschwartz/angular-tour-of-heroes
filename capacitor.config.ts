944af667a38b7eaf98c63359fae2e589d73f82e7
Carl <github@carlpeterschwartz.com>
2022-12-13 12:47:16 +0100

    Commit after Capacitor and Electron installed.

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.carlpeterschwartz.app',
+  appName: 'angular-tour-of-heroes',
+  webDir: 'dist/angular-tour-of-heroes',
+  bundledWebRuntime: false
+};
+
+export default config;
